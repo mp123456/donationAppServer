ad8032765d0df5d4d13837a04f092efedc9c05e7
mp123456 <meravpilip@gmail.com>
2021-03-09 13:17:10 +0200

    Add files via upload

---
+++ Services/DonationService.cs
@@ -0,0 +1,69 @@
+﻿using DonationData.DB;
+using DonationData.Repository;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DonationAPI.Services
+{
+    public class DonationService : IDonationService
+    {
+        private readonly DonationRepository _donationRepository;
+        public DonationService(DonationRepository donationRepository)
+        {
+            _donationRepository = donationRepository;
+        }
+        public async Task<int> CreateDonation(DonationEO donation)
+        {
+            if(donation.Amount>10000)
+            {
+                try
+                {
+                    SendEmail();
+                }
+                catch (Exception ex)
+                {
+
+                }
+                
+            }
+            return await _donationRepository.Create(donation);
+        }
+
+        private void SendEmail()
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("email"));
+            email.To.Add(MailboxAddress.Parse("email"));
+            email.Subject = "התקבלה תרומה חדשה";
+            email.Body = new TextPart(TextFormat.Plain) { Text = "התקבלה תרומה בסך הגדול מ 1000 ש\"ח" };
+
+            // send email
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.gmail.com", 587, true);
+            smtp.Authenticate("email", "password");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+            smtp.Dispose();
+
+
+
+        }
+
+        public async Task<List<DonationEO>> GetList()
+        {
+            return await _donationRepository.GetAll();
+        }
+
+        public async Task<int> UpdateDonation(DonationEO donation)
+        {
+            return await _donationRepository.Update(donation);
+        }
+    }
+}
---
+++ Services/IDonationService.cs
@@ -0,0 +1,17 @@
+﻿using DonationData.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DonationAPI.Services
+{
+    public interface IDonationService
+    {
+        Task<List<DonationEO>> GetList();
+        Task<int> CreateDonation(DonationEO donation);
+        Task<int> UpdateDonation(DonationEO donation);
+
+
+    }
+}
